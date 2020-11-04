e859a0079737c1efd0cf87dd6df4095e2b671aeb
DavidChase88 <68518518+DavidChase88@users.noreply.github.com>
2020-11-04 13:15:47 -0500

    Add files via upload
    Updated file - Deliverable 2 - Visualizations for the Trip Analysis

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,3 +2,6 @@ challenge_table <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFacto
 library(dplyr)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=challenge_table) #generate multiple linear regression model
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=challenge_table)) #generate summary statistics
+library(broom)
+suspension_coil_table <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)
+total_summary <- suspension_coil_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep') #create summary table
